3ce0739b4a439d895e000bf8c568a3e4930e666a
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-09 22:50:38 +0200

    v2.2.1: Creating a Order Actions

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+  const dispatch = useDispatch()
+
   const cart = useSelector((state) => state.cart)
 
   const addDecimals = (num) => {
@@ -17,14 +20,30 @@ const PlaceOrderScreen = () => {
 
   cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
 
-cart.totalPrice = (
-  Number(cart.itemsPrice) + 
-  Number(cart.shippingPrice) +
-  Number(cart.taxPrice)
-).toFixed(2)
+  cart.totalPrice = (Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)).toFixed(2)
+
+  const orderCreate = useSelector((state) => state.orderCreate)
+  const { order, success, error } = orderCreate
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`)
+    }
+    // eslint-disable-next-line
+  }, [history, success])
 
   const placeOrderHandler = () => {
-    console.log('order ');
+    dispatch(
+      createOrder({
+        orderItems: cart.cartITems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.itemsPrice,
+      })
+    )
   }
 
   return (
@@ -76,7 +95,7 @@ cart.totalPrice = (
         </Col>
         <Col md={4}>
           <Card>
-            <ListGroup variant="flush">
+            <ListGroup variant='flush'>
               <ListGroup.Item>
                 <h2>Order Summary</h2>
               </ListGroup.Item>
@@ -98,6 +117,9 @@ cart.totalPrice = (
                   <Col>${cart.taxPrice}</Col>
                 </Row>
               </ListGroup.Item>
+
+              <ListGroup.Item>{error && <Message variant='danger'>{error}</Message>}</ListGroup.Item>
+
               <ListGroup.Item>
                 <Row>
                   <Col>Total</Col>
@@ -105,7 +127,9 @@ cart.totalPrice = (
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button type="button" className='btn-block' disabled={cart.cartItems === 0} onCLick={placeOrderHandler} >Place Order</Button>
+                <Button type='button' className='btn-block' disabled={cart.cartItems === 0} onCLick={placeOrderHandler}>
+                  Place Order
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
